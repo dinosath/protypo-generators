797c2161f731dc71b83cb50e7ee4680302331a2d
Konstantinos Athanasiou <dinosath0@gmail.com>
2024-09-08 13:45:22 +0300

    remove hardcoded user path

--- loco/templates/src/app.rs
+++ loco/templates/src/app.rs
@@ -46,7 +46,6 @@ impl Hooks for App {
     fn routes(_ctx: &AppContext) -> AppRoutes {
         AppRoutes::with_default_routes().prefix("/api")
             .add_route(controllers::auth::routes())
-            .add_route(controllers::user::routes())
     }
 
     fn connect_workers<'a>(_p: &'a mut Processor, _ctx: &'a AppContext) {
