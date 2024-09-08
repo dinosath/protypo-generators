7a5453668a4441df2188cc2e349fb6048ff0ba77
Konstantinos Athanasiou <dinosath0@gmail.com>
2024-09-08 14:51:52 +0300

    remove hardcoded migrations

--- loco/templates/migration/src/lib.rs
+++ loco/templates/migration/src/lib.rs
@@ -2,19 +2,12 @@
 #![allow(clippy::wildcard_imports)]
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_users;
-mod m20240416_071825_roles;
-mod m20240416_082115_users_roles;
-
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_users::Migration),
-            Box::new(m20240416_071825_roles::Migration),
-            Box::new(m20240416_082115_users_roles::Migration),
         ]
     }
 }
