1534da425c04c923fe9aa10c7e3e2ad4b35a257f
Konstantinos Athanasiou <dinosath0@gmail.com>
2024-10-22 10:14:10 +0300

    remove static files from template folder

--- react-admin/templates/src/components/common/action-buttons/editActionButtons.tsx
+++
@@ -1,8 +0,0 @@
-import {TopToolbar, PrevNextButtons, ShowButton} from "react-admin";
-
-export const EditActionButtons = () => (
-    <TopToolbar>
-        <PrevNextButtons linkType="edit"/>
-        <ShowButton/>
-    </TopToolbar>
-);
--- react-admin/templates/src/components/common/action-buttons/showActionButtons.tsx
+++
@@ -1,8 +0,0 @@
-import {TopToolbar, PrevNextButtons, EditButton} from "react-admin";
-
-export const ShowActionButtons = () => (
-    <TopToolbar>
-        <PrevNextButtons linkType="show"/>
-        <EditButton/>
-    </TopToolbar>
-);
